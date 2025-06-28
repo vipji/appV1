47b17b48651741e7cfc26f217fb4640867e6217f
Mr. Vishesh <visheshpandey@Mac.lan>
2025-06-28 20:03:30 +0530

    commit v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-/*
+
 export default defineConfig({
   plugins: [react()],
-  base: "/oopCC/",
+  base: "/oopCC",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
-*/
-export default defineConfig({
-  plugins: [react()],
-  base: "/appV1/", // Must match repository name exactly
-  build: {
-    outDir: 'dist', // Explicit output directory
-    emptyOutDir: true, // Cleans the directory before build
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  // Optional but helpful for debugging:
-  server: {
-    open: true
-  }
 });
